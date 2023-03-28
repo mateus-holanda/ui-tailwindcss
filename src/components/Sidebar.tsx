32793e35a17f30d58405acd076aaaa0ec14e1582
Mateus Holanda <mateus23ita@gmail.com>
2023-03-28 20:57:11 -0300

    Create Link component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { Logo } from "./icons/Logo"
 
 export function Sidebar() {
   return (
-    <aside className="border-r border-white/10 w-80 fixed left-0 top-0 bottom-0 p-6">
+    <aside className="border-r border-white/10 w-80 fixed left-0 top-0 bottom-0 p-6 hidden lg:block">
       <Logo className="text-white h-4 w-auto" />
 
       <nav className="mt-10 flex flex-col gap-6">
@@ -13,13 +13,13 @@ export function Sidebar() {
           </strong>
 
           <div className="text-sm flex flex-col pl-2">
-            <NavLink href="" children="Introduction" />
-            <NavLink href="" children="Quickstart" />
-            <NavLink href="" children="SDKs" />
-            <NavLink href="" children="Authentication" />
-            <NavLink href="" children="Pagination" />
-            <NavLink href="" children="Errors" />
-            <NavLink href="" children="Webhooks"/>
+            <NavLink href="/test/a" children="Introduction" />
+            <NavLink href="/test/b" children="Quickstart" />
+            <NavLink href="/test/c" children="SDKs" />
+            <NavLink href="/test/d" children="Authentication" />
+            <NavLink href="/test/e" children="Pagination" />
+            <NavLink href="/test/f" children="Errors" />
+            <NavLink href="/test/g" children="Webhooks"/>
           </div>
         </div>
 
@@ -29,11 +29,11 @@ export function Sidebar() {
           </strong>
 
           <div className="text-sm flex flex-col pl-2">
-            <NavLink href="" children="Contacts" />
-            <NavLink href="" children="Conversations" />
-            <NavLink href="" children="Messages" />
-            <NavLink href="" children="Groups" />
-            <NavLink href="" children="Attachments" />
+            <NavLink href="/test/h" children="Contacts" />
+            <NavLink href="/test/i" children="Conversations" />
+            <NavLink href="/test/j" children="Messages" />
+            <NavLink href="/test/k" children="Groups" />
+            <NavLink href="/test/l" children="Attachments" />
           </div>
         </div>
       </nav>
