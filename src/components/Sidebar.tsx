2637a2f9261aa6f0c84fc33df43f28aac969ff6e
Mateus Holanda <mateus23ita@gmail.com>
2023-03-28 19:04:11 -0300

    Finish Sidebar CSS

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,42 @@
+import { NavLink } from "./NavLink"
+import { Logo } from "./icons/Logo"
+
 export function Sidebar() {
   return (
-    <aside className="border-r border-white/10 w-80 fixed left-0 top-0 bottom-0 p-6"></aside>
+    <aside className="border-r border-white/10 w-80 fixed left-0 top-0 bottom-0 p-6">
+      <Logo className="text-white h-4 w-auto" />
+
+      <nav className="mt-10 flex flex-col gap-6">
+        <div className="flex flex-col gap-4">
+          <strong className="text-zinc-100 font-medium text-sm">
+            Guides
+          </strong>
+
+          <div className="text-sm flex flex-col pl-2">
+            <NavLink href="" children="Introduction" />
+            <NavLink href="" children="Quickstart" />
+            <NavLink href="" children="SDKs" />
+            <NavLink href="" children="Authentication" />
+            <NavLink href="" children="Pagination" />
+            <NavLink href="" children="Errors" />
+            <NavLink href="" children="Webhooks"/>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <strong className="text-zinc-100 font-medium text-sm">
+            Resources
+          </strong>
+
+          <div className="text-sm flex flex-col pl-2">
+            <NavLink href="" children="Contacts" />
+            <NavLink href="" children="Conversations" />
+            <NavLink href="" children="Messages" />
+            <NavLink href="" children="Groups" />
+            <NavLink href="" children="Attachments" />
+          </div>
+        </div>
+      </nav>
+    </aside>
   )
 }
